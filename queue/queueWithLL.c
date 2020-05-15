8dac6c660dc922be811e7ecd3a711da340bacedd
toledompm <toledompm@gmail.com>
2020-05-14 22:15:07 -0300

    removing double pointers

--- queue/queueWithLL.c
+++ queue/queueWithLL.c
@@ -11,29 +11,29 @@ typedef struct QUEUE{
     llNode * last;
 }queue;
 
-int isQueueEmpty(queue ** q){
-    return ((*q)->last == NULL);
+int isQueueEmpty(queue * q){
+    return (q->last == NULL);
 }
 
-void insertIntoQueue(queue ** q, int v)
+void insertIntoQueue(queue * q, int v)
 {
     llNode * newNode = malloc(sizeof(llNode));
     newNode->val = v;
     newNode->next = NULL;
 
     if(isQueueEmpty(q)){
-        (*q)->first = (*q)->last = newNode;
+        q->first = q->last = newNode;
         return;
     }
     
-    (*q)->last->next = newNode;
-    (*q)->last = newNode;
+    q->last->next = newNode;
+    q->last = newNode;
 }
 
-void removeFromQueue(queue ** q)
+void removeFromQueue(queue * q)
 {
     if(isQueueEmpty(q)) return;
-    (*q)->first = (*q)->first->next;
+    q->first = q->first->next;
 }
 
 void display(llNode * f)
@@ -49,20 +49,21 @@ void display(llNode * f)
 
 int main()
 {
-    queue * q;
+    queue * q = malloc(sizeof(queue));
     q->first = NULL;
     q->last = NULL;
 
-    insertIntoQueue(&q,1);
-    insertIntoQueue(&q,2);
-    insertIntoQueue(&q,3);
+    insertIntoQueue(q,1);
     display(q->first);
-    insertIntoQueue(&q,1);
-    insertIntoQueue(&q,1);
+    insertIntoQueue(q,2);
+    insertIntoQueue(q,3);
     display(q->first);
-    removeFromQueue(&q);
-    removeFromQueue(&q);
+    insertIntoQueue(q,1);
+    insertIntoQueue(q,1);
     display(q->first);
-    insertIntoQueue(&q,3);
+    removeFromQueue(q);
+    removeFromQueue(q);
+    display(q->first);
+    insertIntoQueue(q,3);
     display(q->first);
 }
