94589b2d2b0a7e5e8effc7569a54426775c73598
toledompm <45714268+toledompm@users.noreply.github.com>
2020-07-13 17:01:12 -0300

    Changed partition to compare median of values instead of index
    :(

--- selects/qSort.py
+++ selects/qSort.py
@@ -14,19 +14,22 @@ def medianOfThree(num1,num2,num3):
     return num2
 
 def partition(array, low, high):
-
-    #pivotId = medianOfThree(randint(low, high),randint(low, high),randint(low, high))
-    pivotId = medianOfThree(low,high,high-low//2)
+    fCandidate = randint(low,high) # low
+    sCandidate = randint(low,high) # high
+    tCandidate = randint(low,high) # high-low//2
+    
+    pivotValue = medianOfThree(arr[fCandidate],arr[sCandidate],arr[tCandidate])
+    pivotId = fCandidate if arr[fCandidate] == pivotValue else sCandidate if arr[sCandidate] == pivotValue else tCandidate
 
     array[low], array[pivotId] = array[pivotId], array[low]
-    pivot = array[low]
     i = low + 1
 
     for j in range(i,high+1):
-        if array[j] <= pivot:
+        if array[j] <= pivotValue:
             array[j], array[i] = array[i], array[j]
             i += 1
         j += 1
+
 
     array[low], array[i - 1] = array[i - 1], array[low]
     return i - 1
@@ -39,6 +42,6 @@ def quickSort(arr,low,high):
     quickSort(arr, low, partitionIndex-1)
     quickSort(arr, partitionIndex+1, high)
 
-arr = [0,2,45,1,32,52,235,20452,32,234,1,414,15,12,51,213]
+arr = [0,2123,123,15,44532,235,234,325,235,23,7627,4567,1241,234,1234,231,423,4123,4231,4,1234,124,3124,23,421,4,124,314,13,41,26,342,5634]
 quickSort(arr,0,len(arr)-1) 
 print (arr) 
